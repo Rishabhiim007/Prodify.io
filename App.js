f4e6b2750cb0c1278eadd710cf0a1776bf00f138
Rishabh Srivastav <93145320+Rishabhiim007@users.noreply.github.com>
2022-07-12 22:31:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Info from './components/info/Info';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Info />
+      </div>
+    );
+  }
+}
+
+export default App;
